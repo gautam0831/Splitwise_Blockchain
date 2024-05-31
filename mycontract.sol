46e36d780bce2beb5817b871088c980dbbe474a0
Gautam Pradeep <68078394+gautam0831@users.noreply.github.com>
2024-05-31 08:31:27 -0700

    Update mycontract.sol

--- mycontract.sol
+++ mycontract.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 
-// DO NOT MODIFY BELOW THIS
 pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
 contract Splitwise {
-// DO NOT MODIFY ABOVE THIS
-// ADD YOUR CONTRACT CODE BELOW
   mapping (address => mapping(address => uint32)) private owes; 
   mapping(address => bool) private knownUsers;
   address[] private users;
